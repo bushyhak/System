550c5195bababce0e2cbc6c86b67f5bfea84d795
Gerald Njoroge <onlinemane51@gmail.com>
2023-05-23 12:54:51 +0300

    code added to login form

--- system/views.py
+++ system/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 # Create your views here.
 
@@ -16,5 +18,24 @@ def schedule(request):
     return render(request, 'system/about.html')
 def login(request):
     return render(request, 'system/login.html')
+
+def user_login(request):
+    if request.method =='POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authencticated successfully')
+                else:
+                    return HttpResponse('Disabled account'
+                                        )
+        
+
 
 
