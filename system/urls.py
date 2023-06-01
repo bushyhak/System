21ab6f19e759572f4014e04af7b7afc73b36d93a
bushyhak <onlinemane51@gmail.com>
2023-06-02 00:48:58 +0300

    final homepage

--- system/urls.py
+++ system/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -7,8 +8,10 @@ urlpatterns = [
     path('contact/', views.contact,name='contact'),
     path('book/', views.book, name='book'),
     path('about/', views.about, name='about'),
-    path('login/', views.user_login, name = 'login'),
+    # path('login/', views.user_login, name = 'login'),
     path('register/',views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 
 ]
--- system/views.py
+++ system/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required
+def dashboard(request):
+    return render(request,
+                  'system/dashboard.html',
+                  {'section': 'dashboard'})
 def home(request):
     return render(request,'system/home.html')
 
@@ -16,34 +22,36 @@ def contact(request):
 
 def about(request):
     return render(request, 'system/about.html')
+def logout(request):
+    return render(request, 'system/registration/logout.html')
 
-def login(request):
-    return render(request, 'system/login.html')
+# def login(request):
+#     return render(request, 'system/login.html')
 
 def register(request):
     return render(request, 'system/register.html')
 
-def user_login(request):
-    if request.method =='POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
+# def user_login(request):
+#     if request.method =='POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
 
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authencticated successfully')
-                else:
-                    return HttpResponse('Disabled account'
-                                        )
-            else:
-                return HttpResponse('Invalid login')
-    else:
-        form = LoginForm()
-        return render(request, 'system/login.html', {'form':form})
+#             cd = form.cleaned_data
+#             user = authenticate(request,
+#                                 username=cd['username'],
+#                                 password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Authencticated successfully')
+#                 else:
+#                     return HttpResponse('Disabled account'
+#                                         )
+#             else:
+#                 return HttpResponse('Invalid login')
+#     else:
+#         form = LoginForm()
+#         return render(request, 'system/login.html', {'form':form})
 
 
 
