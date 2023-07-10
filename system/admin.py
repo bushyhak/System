c33c5cc445c4869f4d3939e0d0aa7a1a1710b435
K-Kelvin <kelvinkijanda@gmail.com>
2023-07-10 15:17:24 +0300

    Added filters to Appointment admin

--- system/admin.py
+++ system/admin.py
@@ -4,6 +4,7 @@ from django.utils.html import format_html
 from django.contrib.auth.models import User
 from django.templatetags.static import static
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin.filters import BooleanFieldListFilter, DateFieldListFilter
 from .models import Feedback, Profile, Child, Appointment, Vaccines, Doctor
 
 
@@ -54,6 +55,12 @@ class AppointmentAdmin(admin.ModelAdmin):
         "cancelled_display",
     )
     list_display_links = ("id", "date_and_time")
+    list_filter = (
+        "child",
+        ("completed", BooleanFieldListFilter),
+        ("cancelled", BooleanFieldListFilter),
+        "date",
+    )
 
     def date_and_time_ordering(self, obj):
         return datetime.combine(obj.date, obj.time)
